0bd322be44002387578772536cfe14246c7bb657
Isabel Palomar <isabel.palomarb@gmail.com>
2016-06-22 00:30:45 -0400

    Category and priority spinners added :pill:

--- app/src/main/java/io/androidblog/simpletodo/Utils/Constants.java
+++ app/src/main/java/io/androidblog/simpletodo/Utils/Constants.java
@@ -1,8 +1,13 @@
 package io.androidblog.simpletodo.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 import io.androidblog.simpletodo.BuildConfig;
 
 public class Constants {
 
     public static final String FIREBASE_LOCATION_LIST_ITEMS = "items";
+    public static final List<String> PRIORITY_LIST = Arrays.asList("Low", "Medium", "High");
+
 }
--- app/src/main/java/io/androidblog/simpletodo/activities/AddItemActivity.java
+++ app/src/main/java/io/androidblog/simpletodo/activities/AddItemActivity.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.androidblog.simpletodo.R;
 import io.androidblog.simpletodo.SimpleTodoApplication;
+import io.androidblog.simpletodo.models.Category;
 import io.androidblog.simpletodo.models.Item;
+import io.androidblog.simpletodo.utils.Constants;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -23,6 +29,10 @@ public class AddItemActivity extends AppCompatActivity {
     EditText editTextNoteDesc;
     @Bind(R.id.fab)
     FloatingActionButton fab;
+    @Bind(R.id.spinnerCategory)
+    Spinner spinnerCategory;
+    @Bind(R.id.spinnerPriority)
+    Spinner spinnerPriority;
 
     private DatabaseReference databaseReference;
 
@@ -36,10 +46,32 @@ public class AddItemActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         SimpleTodoApplication app = (SimpleTodoApplication) getApplicationContext();
-
         databaseReference = app.getItemsReference();
 
+        setCategorySpinnerData();
+        setPrioritySpinnerData();
+
     }
+
+    private void setPrioritySpinnerData() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, Constants.PRIORITY_LIST);
+        spinnerPriority.setAdapter(adapter);
+    }
+
+    private void setCategorySpinnerData() {
+
+        ArrayList<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Home", "home"));
+        categoryList.add(new Category(2, "Travel", "travel"));
+        categoryList.add(new Category(3, "Groceries", "groceries"));
+        categoryList.add(new Category(4, "Work", "work"));
+        categoryList.add(new Category(5, "General", "general"));
+
+        ArrayAdapter<Category> adapter = new ArrayAdapter<Category>(this, android.R.layout.simple_spinner_dropdown_item, categoryList);
+        spinnerCategory.setAdapter(adapter);
+
+    }
+
 
     @OnClick(R.id.fab)
     public void onClick() {
