8919d6c8732c4b1cc8226a50739bd7f3564b8c15
Isabel Palomar <isabel.palomarb@gmail.com>
2016-07-13 18:40:27 -0400

    Launcher Acitivity

---
+++ app/src/main/java/io/androidblog/simpletodo/activities/LauncherActivity.java
@@ -0,0 +1,27 @@
+package io.androidblog.simpletodo.activities;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import io.androidblog.simpletodo.R;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_launcher);
+
+    }
+
+}
