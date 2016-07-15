cf330d3019647742017f28c5cadd6b5ed84366ad
Isabel Palomar <isabel.palomarb@gmail.com>
2016-07-15 15:16:06 -0400

    Styles for launcher Activity

--- app/src/main/java/io/androidblog/simpletodo/activities/LauncherActivity.java
+++ app/src/main/java/io/androidblog/simpletodo/activities/LauncherActivity.java
@@ -1,19 +1,16 @@
 package io.androidblog.simpletodo.activities;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
 
 import io.androidblog.simpletodo.R;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+
 public class LauncherActivity extends AppCompatActivity {
 
     @Override
@@ -21,7 +18,33 @@ public class LauncherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_launcher);
-
+        setCursiveFont();
+        startMainActivity();
     }
 
+    private void setCursiveFont() {
+        String fontPath = "fonts/Allura-Regular.ttf";
+        TextView txtTitle = (TextView) findViewById(R.id.fullscreen_content);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+        txtTitle.setTypeface(tf);
+    }
+
+    private void startMainActivity() {
+
+        //Initiates the MainActivity after 1000L
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LauncherActivity.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+
+        }, 1600L);
+
+    }
 }
